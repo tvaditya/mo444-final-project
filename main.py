563b9f9c77ac6937d4883cdb2dc87c0222ee6c3c
jose.pereira <jose.pereira@sofist.com.br>
2015-11-08 20:26:14 -0200

    Refacoting of the variables names.

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ import pandas as pd
 import print_functions, vectorization, data_helper#, tfidf
 from constants import *
 
-train_size = 1000 # 15000
+size = 1000 # 15000
 
 filename = "out-july-2015-dou.out"
 
 def clean_text():
     csv = data_helper.read_csv(raw_directory + filename)
 
-    # Slice the data to get the training data
-    train = csv[:][:train_size]
+    # Slice the data to clean the text
+    data = csv[:][:size]
 
-    clean_description_list = data_helper.clean_list(train)
-    train[integra_index] = clean_description_list
-    data_helper.save_file(train, clean_text_directory, filename)
+    clean_description_list = data_helper.clean_list(data)
+    data[integra_index] = clean_description_list
+    data_helper.save_file(data, clean_text_directory, filename)
 
 def perform_vectorization():
     csv = data_helper.read_csv(clean_text_directory + filename)
-    clean_description_list = csv[integra_index][:train_size]
+    clean_description_list = csv[integra_index][:size]
     train_data_features, vocab = vectorization.create_bag_of_words(clean_description_list)
     print_functions.print_examples(clean_description_list, train_data_features)
     print_functions.print_vocabulary(vocab)
 
 
-print "Train size: " + str(train_size)
+print "Train size: " + str(size)
 clean_text()
 perform_vectorization()
