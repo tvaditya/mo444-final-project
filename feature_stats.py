b7d670c4b71a9418c81200530bdf61d8351bc20a
jose.pereira <jose.pereira@sofist.com.br>
2015-11-14 20:14:16 -0200

    Refactoring of the feature_stats

--- feature_stats.py
+++ feature_stats.py
@@ -3,12 +3,18 @@ __author__="jose"
 from constants import *
 import data_helper as dh
 
-csv_features = dh.read_csv(features_directory + filename)
 
-size_entries = csv_features.shape[0]
-entries = csv_features.groupby([0])
+def print_stats(source_file, columns):
+    csv_features = dh.read_csv(source_file)
 
-for i in range(0, len(entries.size())):
-    entry = entries.size()[i]
-    print entry / float(size_entries)
+    size_entries = len(csv_features)
+    entries = csv_features.groupby(columns)
 
+    for name, group in entries:
+        print "Group Name: " + str(name)
+        entry = len(group)
+        print entry / float(size_entries)
+
+
+print_stats(features_directory + filename, [0])
+print_stats(clean_text_directory + filename, [diario_index])
